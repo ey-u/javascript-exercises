c3b1341ef41ab6b475810f69b8d72997dfe9b992
ey-u <175895848+ey-u@users.noreply.github.com>
2024-08-14 21:12:06 -0400

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (list, ...items) {
+  for (let item of items) {
+    let index = list.indexOf(item);
+    while (index >= 0) {
+      list.splice(index, 1);
+      index = list.indexOf(item);
+    }
+  }
+  return list;
 };
 
 // Do not edit below this line
