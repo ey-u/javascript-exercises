a5bd18cf610cb391890e92b232f7e238641505bc
ey-u <175895848+ey-u@users.noreply.github.com>
2024-08-14 21:21:21 -0400

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+  if (
+    start < 0 ||
+    end < 0 ||
+    !Number.isInteger(start) ||
+    !Number.isInteger(end)
+  ) {
+    return "ERROR";
+  }
+  if (end < start) {
+    let temp = start;
+    start = end;
+    end = temp;
+  }
+  let result = 0;
+  for (let i = start; i <= end; i++) result += i;
+  return result;
 };
 
 // Do not edit below this line
