c816ee074d2751172ccb80fef36cfa889beb097f
ey-u <175895848+ey-u@users.noreply.github.com>
2024-08-14 20:48:48 -0400

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let characters = string.split("");
+  let result = "";
+  for (let i = characters.length - 1; i >= 0; i--) {
+    result += characters[i];
+  }
+  return result;
 };
 
 // Do not edit below this line
