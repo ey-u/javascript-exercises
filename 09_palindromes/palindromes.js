e53742f0fe3f9ca4085658fe3bf38d1d100926f3
ey-u <175895848+ey-u@users.noreply.github.com>
2024-08-20 14:22:36 -0400

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let noPunctuation = string
+    .toLowerCase()
+    .replace(/[^\w\s\']|_/g, "")
+    .replaceAll(" ", "");
+  let reverse = noPunctuation.split("").reverse().join("");
+  return noPunctuation === reverse;
 };
 
 // Do not edit below this line
