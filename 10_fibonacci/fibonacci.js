c3e3165d4a09e94065f6ea8ea9fa0bb44224ed21
ey-u <175895848+ey-u@users.noreply.github.com>
2024-08-20 14:22:43 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (place) {
+  let arg = place;
+  if (typeof place !== "number") arg = parseInt(place);
+  if (arg < 0) return "OOPS";
 
+  let total = 0;
+  let curr = 1;
+  let prev = 0;
+  for (i = 0; i < arg; i++) {
+    total = curr + prev;
+    prev = curr;
+    curr = total;
+  }
+  return prev;
 };
 
 // Do not edit below this line
